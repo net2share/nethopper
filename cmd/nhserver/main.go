fb0ef7d97637b69acec9877f4ec0c9f83d34d549
crazydi4mond <255249920+crazydi4mond@users.noreply.github.com>
2026-03-09 02:30:17 +0100

    fix: add cmd/ directories excluded by gitignore

---
+++ cmd/nhserver/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/net2share/nethopper/internal/cmd"
+)
+
+var (
+	version   = "dev"
+	buildTime = "unknown"
+)
+
+func main() {
+	root := cmd.NewServerRoot(version, buildTime)
+	if err := root.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
